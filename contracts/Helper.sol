5e89ef8962dafe8b60a4b9a97b111f97cc472b16
NDW <cryptondee@gmail.com>
2022-11-08 12:00:50 +0100

    first push

---
+++ contracts/Helper.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.4;
+
+contract Helper {
+    uint public num;
+
+    function setNum(uint _num) public {
+        num = _num;
+    }
+}
---
+++ test/attack.js
@@ -0,0 +1,26 @@
+const { expect } = require("chai");
+const { BigNumber } = require("ethers");
+const { ethers, waffle } = require("hardhat");
+
+describe("Attack", function () {
+  it("should change the owner of the good  contract", async function () {
+    const helperContract = await ethers.getContractFactory("Helper");
+    const _helperContract = await helperContract.deploy();
+    await _helperContract.deployed();
+    console.log(`Helper contract address: ${_helperContract.address}`);
+
+    const goodContract = await ethers.getContractFactory("Good");
+    const _goodContract = await goodContract.deploy(_helperContract.address);
+    await _goodContract.deployed();
+    console.log(`Good contract address:" ${_goodContract.address}`);
+
+    const attackContract = await ethers.getContractFactory("Attack");
+    const _attackContract = await attackContract.deploy(_goodContract.address);
+    await _attackContract.deployed();
+    console.log(`Attack contract address: ${_attackContract.address}`);
+
+    let tx = await _attackContract.attack();
+    await tx.wait();
+    expect(await _goodContract.owner()).to.equal(_attackContract.address);
+  });
+});
